2fd5fb6f7aaf9cab1b2828ab897cd00cb84d98fd
Ubuntu <ubuntu@ip-172-31-92-70.ec2.internal>
2018-11-11 06:31:42 +0000

    minor

--- server.py
+++ server.py
@@ -502,4 +502,5 @@ def gradcam(category_id_1,category_id_2,data_id_1):
 
 #################################################################################
 if __name__ == '__main__':
-    app.run()
+    #app.run()
+    app.run(debug=False, host='0.0.0.0', port=80)
